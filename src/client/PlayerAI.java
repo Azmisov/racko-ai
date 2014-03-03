a4aa261dd98895fd205a0a290f6e8145197a3ebf
ctmcneill <ctmcneill@gmail.com>
2014-03-03 15:19:39 -0700

    Fixed some issues.

--- src/client/PlayerAI.java
+++ src/client/PlayerAI.java
@@ -24,40 +24,18 @@ public class PlayerAI extends Player{
 		rand = new Random();
 	}
 	
-	@Override
-<<<<<<< 16426c1ff2ea805a8700b834d1a77e47e61b636f
 	public int play() {
-		//throw new UnsupportedOperationException("Not supported yet.");
-		
-		boolean drawFromDiscard = rand.nextBoolean();
-=======
-	public void play() {
 		boolean drawFromDiscard = decideDraw();//
->>>>>>> ccc2c7878dd13ace2aafd10abfd6874b3e62169d
 		
 		int card = game.deck.draw(drawFromDiscard);
 		
-		//System.out.println(playerNumber + ": drew card number: " + card);
-		
-<<<<<<< 16426c1ff2ea805a8700b834d1a77e47e61b636f
-		//DataInstance thisMove = new DataInstance(rack.getCards(), );
-		
-		int slot = rand.nextInt(game.rack_size+1) - 1;
-=======
+		System.out.println(playerNumber + ": drew card number: " + card);
 		int slot = decidePlay(rack, card);
->>>>>>> ccc2c7878dd13ace2aafd10abfd6874b3e62169d
 		
 		int discard = slot == -1 ? card : rack.swap(card, slot, false);
 		
-		//System.out.println(playerNumber + ": rack: " + rack.toString());
-		
-<<<<<<< 16426c1ff2ea805a8700b834d1a77e47e61b636f
-		//addMoveToHistory(thisMove);
-		
-		//System.out.println(playerNumber + ": discarded card number: " + discard);
-=======
+		System.out.println(playerNumber + ": rack: " + rack.toString());
 		System.out.println(playerNumber + ": discarded card number: " + discard);
->>>>>>> ccc2c7878dd13ace2aafd10abfd6874b3e62169d
 		
 		return discard;
 	}
@@ -89,14 +67,8 @@ public class PlayerAI extends Player{
 		System.out.println(playerNumber +": "+(won ? "WON" : "LOST")+" GAME, score = "+score);
 	}
 	
-<<<<<<< 16426c1ff2ea805a8700b834d1a77e47e61b636f
-	
-	
-	private void addMoveToHistory(DataInstance move){
-=======
 	private void addMoveToHistory(DrawDataInstance move)
 	{
->>>>>>> ccc2c7878dd13ace2aafd10abfd6874b3e62169d
 		playHistory.add(move);
 	}
 	
